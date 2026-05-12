0f1e04ad03333e5de644b4c954914b309ce9d279
ashok-aroha-code <ashok.mamnani@aascentinfo.solutions>
2026-05-12 15:34:33 +0530

    removed lagacy code

--- legacy/AACR 2026/check.py
+++
@@ -1,6 +0,0 @@
-def reverse_str(sentence):
-    words = sentence.split()
-    words.reverse()
-    return " ".join(words)
-text = "i love tulli"
-print
--- legacy/AACR 2026/practice.py
+++
@@ -1,8 +0,0 @@
-def linear_search(arr, x):
-    for i in range(len(arr)):
-        if arr[i] == x:
-            return i
-    return -1
-arr = [1, 12, 3, 45, 5, 67]
-x = 67
-print(linear_search(arr, x))    
